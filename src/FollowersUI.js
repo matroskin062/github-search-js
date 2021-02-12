3c76f4c6c09c4458c4de0d62c01a071b2a666d2b
Dima Matroskin <dima.matroskin062@gmail.com>
2021-02-12 18:18:35 +0200

    no repos and no followers messages added

--- src/FollowersUI.js
+++ src/FollowersUI.js
@@ -5,37 +5,41 @@ class FollowersUI {
   constructor() {
     this.followersContainer = document.querySelector('.followers');
     this.followersCards = document.querySelector('.followers__cards');
-    this.errorText = document.querySelector('.profile .error h3');
-    this.error = document.querySelector('.profile .error');
+    this.error = document.querySelector('.followers .error');
     this.registerListeners();
   }
 
   renderFollowerList() {
     this.followersCards.innerHTML = '';
 
-    User.followers.map((user) => {
-      const card = document.createElement('div');
-      card.classList.add('followers__card', 'card');
+    if (User.followers.length) {
+      User.followers.map((user) => {
+        const card = document.createElement('div');
+        card.classList.add('followers__card', 'card');
 
-      const avatar = document.createElement('img');
-      avatar.src = user.avatar_url;
+        const avatar = document.createElement('img');
+        avatar.src = user.avatar_url;
 
-      const usernameLink = document.createElement('a');
-      usernameLink.href = user.html_url;
-      usernameLink.textContent = user.login;
-      usernameLink.target = 'blank';
+        const usernameLink = document.createElement('a');
+        usernameLink.href = user.html_url;
+        usernameLink.textContent = user.login;
+        usernameLink.target = 'blank';
 
-      const usernameLinkWrapper = document.createElement('div');
+        const usernameLinkWrapper = document.createElement('div');
 
-      card.append(avatar);
-      card.append(usernameLinkWrapper);
-      usernameLinkWrapper.append(usernameLink);
-      this.followersCards.append(card);
-    });
+        card.append(avatar);
+        card.append(usernameLinkWrapper);
+        usernameLinkWrapper.append(usernameLink);
+        this.followersCards.append(card);
+      });
+    } else {
+      this.error.classList.remove('hide');
+    }
   }
 
   registerListeners() {
     emitter.subscribe('dataLoaded', () => {
+      this.error.classList.add('hide');
       this.followersContainer.classList.add('hide');
       this.renderFollowerList();
     });
--- src/ReposUI.js
+++ src/ReposUI.js
@@ -7,55 +7,57 @@ class ReposUI {
   constructor() {
     this.reposContainer = document.querySelector('.repos');
     this.reposCards = document.querySelector('.repos__cards');
-    this.errorText = document.querySelector('.profile .error h3');
-    this.error = document.querySelector('.profile .error');
+    this.error = document.querySelector('.repos .error');
     this.registerListeners();
   }
 
   renderReposList() {
     this.reposCards.innerHTML = '';
+    if (User.repos.length) {
+      User.repos.map((repo) => {
+        const card = document.createElement('div');
+        card.classList.add('repos__card', 'card');
 
-    User.repos.map((repo) => {
-      const card = document.createElement('div');
-      card.classList.add('repos__card', 'card');
+        const repoIco = document.createElement('img');
+        repoIco.src = ico;
 
-      const repoIco = document.createElement('img');
-      repoIco.src = ico;
+        const repoInfo = document.createElement('div');
+        repoInfo.classList.add('repos__card-info');
 
-      const repoInfo = document.createElement('div');
-      repoInfo.classList.add('repos__card-info');
+        const cardTitleLink = document.createElement('a');
+        cardTitleLink.textContent = repo.name;
+        cardTitleLink.href = repo.html_url;
+        cardTitleLink.target = 'blank';
 
-      const cardTitleLink = document.createElement('a');
-      cardTitleLink.textContent = repo.name;
-      cardTitleLink.href = repo.html_url;
-      cardTitleLink.target = 'blank';
+        card.append(repoIco);
+        card.append(repoInfo);
+        repoInfo.append(cardTitleLink);
 
-      card.append(repoIco);
-      card.append(repoInfo);
-      repoInfo.append(cardTitleLink);
+        if (repo.description) {
+          const repoDesc = document.createElement('p');
+          repoDesc.textContent = repo.description;
+          repoInfo.append(repoDesc);
+        }
 
-      if (repo.description) {
-        const repoDesc = document.createElement('p');
-        repoDesc.textContent = repo.description;
-        repoInfo.append(repoDesc);
-      }
+        if (repo.language) {
+          const langBlock = document.createElement('div');
+          langBlock.classList.add('language');
 
-      if (repo.language) {
-        const langBlock = document.createElement('div');
-        langBlock.classList.add('language');
+          const circle = document.createElement('span');
+          circle.style.backgroundColor = getLangColor(repo.language);
 
-        const circle = document.createElement('span');
-        circle.style.backgroundColor = getLangColor(repo.language);
+          const title = document.createElement('p');
+          title.textContent = repo.language;
 
-        const title = document.createElement('p');
-        title.textContent = repo.language;
+          langBlock.append(circle, title);
+          repoInfo.append(langBlock);
+        }
 
-        langBlock.append(circle, title);
-        repoInfo.append(langBlock);
-      }
-
-      this.reposCards.append(card);
-    });
+        this.reposCards.append(card);
+      });
+    } else {
+      this.error.classList.remove('hide');
+    }
   }
 
   render() {
@@ -68,6 +70,7 @@ class ReposUI {
 
   registerListeners() {
     emitter.subscribe('dataLoaded', () => {
+      this.error.classList.add('hide');
       this.render();
       this.renderReposList();
     });
